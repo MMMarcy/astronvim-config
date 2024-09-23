10166bb942aad22aef36ff1a4928f49643f7c52d
Marcello Steiner <steiner.marcello@gmeil.com>
2024-09-23 15:16:25 +0200

    Last commit.

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,17 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.terraform" },
+  { import = "astrocommunity.pack.yaml" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.workflow.hardtime-nvim" },
 }
